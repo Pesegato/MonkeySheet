1fb6be112cef9f623e94581a7f41f5a12c3d19be
Pesegato <pesegato@outlook.com>
2017-03-03 17:50:44 +0100

    Removed transform logic from BGeometry

--- src/main/java/com/pesegato/MonkeySheet/batch/BGeometry.java
+++ src/main/java/com/pesegato/MonkeySheet/batch/BGeometry.java
@@ -1,6 +1,5 @@
 package com.pesegato.MonkeySheet.batch;
 
-import com.jme3.math.FastMath;
 import com.jme3.math.Vector2f;
 
 import java.nio.FloatBuffer;
@@ -9,15 +8,14 @@ import java.nio.IntBuffer;
 public class BGeometry {
 
     public float QUAD_SIZE = 1;
-    float scale = 1;
     float actualSize;
+    BTransform transform = new BTransform();
 
     int bufPosition;
-    Vector2f center=new Vector2f();
-    Vector2f ll=new Vector2f();
-    Vector2f lr=new Vector2f();
-    Vector2f ur=new Vector2f();
-    Vector2f ul=new Vector2f();
+    Vector2f ll = new Vector2f();
+    Vector2f lr = new Vector2f();
+    Vector2f ur = new Vector2f();
+    Vector2f ul = new Vector2f();
 
     FloatBuffer vertexData, msPosData, alphaData;
     IntBuffer idxData;
@@ -77,21 +75,9 @@ public class BGeometry {
 
     public void setQuadSize(float size) {
         QUAD_SIZE = size;
-        actualSize=QUAD_SIZE*scale;
     }
 
-    public void setLocalScale(float scale){
-        this.scale=scale;
-        actualSize=QUAD_SIZE*scale;
-        updateBuffer();
-    }
-
-    float angle=0;
-    public void setLocalRotation(float angle){
-        this.angle=angle;
-    }
-
-    public void removeFromParent(){
+    public void removeFromParent() {
         idxData.position(bufPosition * 6);
         idxData.put(0);
         idxData.put(0);
@@ -101,68 +87,71 @@ public class BGeometry {
         idxData.put(0);
     }
 
-    public void move(float x, float y) {
-        center.x += x;
-        center.y += y;
-        updateBuffer();
+    public BTransform getTransform() {
+        return transform;
     }
 
-    public Vector2f getLocalTranslation(){
-        return center;
-    }
-
-    public void setPosition(float x, float y) {
-        center.x = x;
-        center.y = y;
-        updateBuffer();
-    }
-
-    private void updateBuffer() {
+    public void applyTransform() {
         vertexData.position(bufPosition * 12);
 
-        ll.x=(-actualSize / 2 + center.x);
-        ll.y=(-actualSize / 2 + center.y);
+        actualSize = QUAD_SIZE * transform.scale;
+        ll.x = (-actualSize / 2);
+        ll.y = (-actualSize / 2);
 
-        lr.x=(actualSize / 2 + center.x);
-        lr.y=(-actualSize / 2 + center.y);
+        lr.x = (actualSize / 2);
+        lr.y = (-actualSize / 2);
 
-        ul.x=(-actualSize / 2 + center.x);
-        ul.y=(actualSize / 2 + center.y);
+        ul.x = (-actualSize / 2);
+        ul.y = (actualSize / 2);
 
-        ur.x=(actualSize / 2 + center.x);
-        ur.y=(actualSize / 2 + center.y);
+        ur.x = (actualSize / 2);
+        ur.y = (actualSize / 2);
 
-        float cos=FastMath.cos(angle);
-        float sin=FastMath.sin(angle);
+        manage(ll);
+        manage(lr);
+        manage(ul);
+        manage(ur);
+        /*
+        ll.subtractLocal(transform.offset);
+        ll.rotateAroundOrigin(transform.angle, false);
+        ll.addLocal(transform.offset);
+        ll.addLocal(transform.center);
 
-        vertices[0] = ll.x*cos-ll.y*sin;
-        vertices[1] = ll.x*sin+ll.y*cos;
-        vertices[2] = 0;
-        vertices[3] = lr.x*cos-lr.y*sin;
-        vertices[4] = lr.x*sin+lr.y*cos;
-        vertices[5] = 0;
-        vertices[6] = ul.x*cos-ul.y*sin;
-        vertices[7] = ul.x*sin+ul.y*cos;
-        vertices[8] = 0;
-        vertices[9] = ur.x*cos-ur.y*sin;
-        vertices[10] = ur.x*sin+ur.y*cos;
-        vertices[11] = 0;
-        vertexData.put(vertices, 0, 12);
+        lr.subtractLocal(transform.offset);
+        lr.rotateAroundOrigin(transform.angle, false);
+        lr.addLocal(transform.offset);
+        lr.addLocal(transform.center);
 
-/*
-        vertices[0] = -actualSize / 2 + x;
-        vertices[1] = -actualSize / 2 + y;
-        vertices[2] = 0;
-        vertices[3] = actualSize / 2 + x;
-        vertices[4] = -actualSize / 2 + y;
-        vertices[5] = 0;
-        vertices[6] = -actualSize / 2 + x;
-        vertices[7] = actualSize / 2 + y;
-        vertices[8] = 0;
-        vertices[9] = actualSize / 2 + x;
-        vertices[10] = actualSize / 2 + y;
-        vertices[11] = 0;
-        vertexData.put(vertices, 0, 12);
+        ul.subtractLocal(transform.offset);
+        ul.rotateAroundOrigin(transform.angle, false);
+        ul.addLocal(transform.offset);
+        ul.addLocal(transform.center);
+
+        ur.subtractLocal(transform.offset);
+        ur.rotateAroundOrigin(transform.angle, false);
+        ur.addLocal(transform.offset);
+        ur.addLocal(transform.center);
         */
+
+        vertices[0] = ll.x;
+        vertices[1] = ll.y;
+        vertices[2] = 0;
+        vertices[3] = lr.x;
+        vertices[4] = lr.y;
+        vertices[5] = 0;
+        vertices[6] = ul.x;
+        vertices[7] = ul.y;
+        vertices[8] = 0;
+        vertices[9] = ur.x;
+        vertices[10] = ur.y;
+        vertices[11] = 0;
+        vertexData.put(vertices, 0, 12);
+    }
+
+    private void manage(Vector2f vx){
+        vx.subtractLocal(transform.offset);
+        vx.rotateAroundOrigin(transform.angle, false);
+        vx.addLocal(transform.offset);
+        vx.addLocal(transform.center);
     }
 }
