aa73d656d3fabb271e27e5b4b5085e7ff599d1a4
Pesegato <pesegato@outlook.com>
2017-04-10 16:39:05 +0200

    Added path customization

--- src/main/java/com/pesegato/MonkeySheet/MSContainer.java
+++ src/main/java/com/pesegato/MonkeySheet/MSContainer.java
@@ -8,19 +8,24 @@ package com.pesegato.MonkeySheet;
 import com.pesegato.goldmonkey.Container;
 
 /**
- *
  * @author Pesegato
  */
 public class MSContainer {
 
-    boolean USE_COMPRESSION=false;
+    boolean USE_COMPRESSION = false;
 
     public int numTiles;
     String[] sheets;
+    String name;
 
-    public MSContainer(String name){
-        Container c=MonkeySheetAppState.getContainer(name);
-        this.numTiles= c.size;
-        this.sheets=new String[]{"Textures/"+name+(USE_COMPRESSION?".dds":".png")};
+    public MSContainer(String name) {
+        Container c = MonkeySheetAppState.getContainer(name);
+        this.numTiles = c.size;
+        this.name = name;
+        this.sheets = new String[]{"Textures/" + name + (USE_COMPRESSION ? ".dds" : ".png")};
+    }
+
+    public void setPath(String path) {
+        this.sheets = new String[]{path + name + (USE_COMPRESSION ? ".dds" : ".png")};
     }
 }
