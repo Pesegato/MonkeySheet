0807f86e7291a7c1b44184ee99c1913b5473454a
Pesegato <pesegato@outlook.com>
2018-04-11 17:40:53 +0200

    Improved API, fix size rect

--- src/main/java/com/pesegato/collision/hitbox/HBRect.java
+++ src/main/java/com/pesegato/collision/hitbox/HBRect.java
@@ -8,7 +8,9 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Quad;
+import com.pesegato.collision.DebuggableBody;
 import com.pesegato.collision.Dyn4JShapeControl;
+import org.dyn4j.dynamics.BodyFixture;
 import org.dyn4j.geometry.Convex;
 import org.dyn4j.geometry.MassType;
 import org.dyn4j.geometry.Rectangle;
@@ -16,47 +18,54 @@ import org.dyn4j.geometry.Rectangle;
 import static com.pesegato.MonkeySheet.MSGlobals.SHOW_HITBOX;
 import static com.pesegato.MonkeySheet.MSGlobals.SPRITE_SIZE;
 
-public class HBRect {
+public class HBRect extends DebuggableBody {
     String name = "";
     float w, h;
     public long id;
 
-    public HBRect(long id, float w, float h) {
+    public HBRect(long id, float wP, float hP) {
         this.id = id;
-        this.w = w * SPRITE_SIZE;
-        this.h = h * SPRITE_SIZE;
+        this.w = wP * SPRITE_SIZE;
+        this.h = hP * SPRITE_SIZE;
+        addFixture(new BodyFixture(new Rectangle(new Float(w), new Float(h))));
     }
 
-    public HBRect(String name, long id, float w, float h) {
+    public HBRect(String name, long id, float wP, float hP) {
         this.name = name;
         this.id = id;
-        this.w = w * SPRITE_SIZE;
-        this.h = h * SPRITE_SIZE;
+        this.w = wP * SPRITE_SIZE;
+        this.h = hP * SPRITE_SIZE;
+        addFixture(new BodyFixture(new Rectangle(new Float(w), new Float(h))));
     }
 
+    @Deprecated
     public Node getNode(AssetManager assetM, ColorRGBA color) {
         Node n = new Node();
         if (SHOW_HITBOX) {
-            //geo.setCullHint(Spatial.CullHint.Always);
-            //}else {
-            Material mat = new Material(assetM, "Common/MatDefs/Misc/Unshaded.j3md");
-            ColorRGBA col = color.clone();
-            col.a = .5f;
-            mat.setColor("Color", col);
-            mat.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-            Geometry geo = new Geometry(name, new Quad(w, h));
-            geo.setMaterial(mat);
-            geo.setLocalTranslation(-w / 2, -h / 2, 0);
-            n.attachChild(geo);
+            n.attachChild(makeHitboxMarker(assetM, n, color));
         }
         return n;
     }
 
-    public Convex getConvex(){
+    public Convex getConvex() {
         return new Rectangle(new Float(w), new Float(h));
     }
 
+    @Deprecated
     public Dyn4JShapeControl getControl() {
         return new Dyn4JShapeControl(getConvex(), MassType.INFINITE, this);
+    }
+
+    @Override
+    public Geometry makeHitboxMarker(AssetManager assetManager, Node n, ColorRGBA color) {
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        ColorRGBA col = color.clone();
+        col.a = .5f;
+        mat.setColor("Color", col);
+        mat.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
+        Geometry geo = new Geometry(name, new Quad(w, h));
+        geo.setMaterial(mat);
+        geo.setLocalTranslation(-w / 2, -h / 2, 0);
+        return geo;
     }
 }
