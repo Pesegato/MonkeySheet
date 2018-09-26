d1c6b7c9f7663d1cd0a204fff17fa2216444a9ce
Pesegato <pesegato@outlook.com>
2018-09-26 15:41:01 +0200

    Fix incompatibility

--- src/main/java/com/pesegato/MonkeySheet/batch/BGeometryBodyControl.java
+++ src/main/java/com/pesegato/MonkeySheet/batch/BGeometryBodyControl.java
@@ -11,8 +11,8 @@ public abstract class BGeometryBodyControl extends BGeometryControl {
     public float offsetAngle;
     protected Body body;
 
-    protected BGeometryBodyControl(Body body, BGeometry bgeo, float duration) {
-        super(bgeo, duration);
+    protected BGeometryBodyControl(Body body, BGeometry bgeo) {
+        super(bgeo);
         this.body = body;
     }
 
@@ -28,18 +28,10 @@ public abstract class BGeometryBodyControl extends BGeometryControl {
                 (float) body.getTransform().getTranslationY() + offsetY);
         bgeo.getTransform().setLocalRotation((float) body.getTransform().getRotation() + offsetAngle);
         bgeo.applyTransform();
-        duration -= tpf;
-        if (duration < 0) {
-            setEnabled(false);
-        }
+        //duration -= tpf;
+        //if (duration < 0) {
+        //   setEnabled(false);
+        //}
     }
 
-    @Override
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-        if (!enabled) {
-            bgeo.removeFromParent();
-            spatial.removeControl(this);
-        }
-    }
 }
