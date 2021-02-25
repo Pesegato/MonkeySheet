2fba7d2aeeb1f3e7273eb638ed7a70a3cec1943e
Pesegato <pesegato@outlook.com>
2021-02-25 12:25:00 +0100

    Reformat code

--- src/main/java/com/pesegato/MonkeySheet/MSMaterialControl.java
+++ src/main/java/com/pesegato/MonkeySheet/MSMaterialControl.java
@@ -18,7 +18,6 @@ import com.jme3.texture.Texture;
 import static com.pesegato.MonkeySheet.MonkeySheetAppState.log;
 
 /**
- *
  * @author Pesegato
  */
 public class MSMaterialControl extends AbstractControl {
@@ -29,43 +28,43 @@ public class MSMaterialControl extends AbstractControl {
     public int position;
     MSControl msc;
 
-    private float alphaValue=1;
-    private boolean flipped=false;
+    private float alphaValue = 1;
+    private boolean flipped = false;
     private float hueShift = 0;
-    private ColorRGBA fogColor=ColorRGBA.Pink;
-    private float fogIntensity=0;
+    private ColorRGBA fogColor = ColorRGBA.Pink;
+    private float fogIntensity = 0;
 
-    public MSMaterialControl(AssetManager assetManager, Geometry geo, MSContainer msCont, MSControl msc){
+    public MSMaterialControl(AssetManager assetManager, Geometry geo, MSContainer msCont, MSControl msc) {
         material = new Material(assetManager, "MonkeySheet/MatDefs/Anim.j3md");
-        Texture[] sheetsX=new Texture[msCont.sheets.length];
+        Texture[] sheetsX = new Texture[msCont.sheets.length];
         for (int i = 0; i < msCont.sheets.length; i++) {
             long start = System.currentTimeMillis();
             long end;
-            System.out.println("MonkeySheet: Now loading "+msCont.sheets[i]);
-            sheetsX[i]=assetManager.loadTexture(msCont.sheets[i]);
+            System.out.println("MonkeySheet: Now loading " + msCont.sheets[i]);
+            sheetsX[i] = assetManager.loadTexture(msCont.sheets[i]);
             end = System.currentTimeMillis();
             log.trace("loaded {}", (end - start));
         }
         material.setFloat("SizeX", msCont.numTiles);
         material.setFloat("SizeY", msCont.numTiles);
         material.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        for (MTween mt:MonkeySheetAppState.anis.values()){
-            if (mt.msCont==msCont)
+        for (MTween mt : MonkeySheetAppState.anis.values()) {
+            if (mt.msCont == msCont)
                 mt.setTextures(sheetsX);
         }
         geo.setMaterial(material);
         geo.addControl(this);
-        this.msc=msc;
-            material.setFloat("Position", msc.anim.anim[msc.position].position);
-            material.setTexture("ColorMap", msc.anim.anim[msc.position].sheetX);
-            material.setFloat("FlipHorizontal", 0.0f);
-            material.setFloat("AlphaValue", 1.0f);
-            material.setColor("FogColor", fogColor);
-            material.setFloat("FogIntensity", 0.0f);
-            material.setFloat("HueShift", hueShift);
+        this.msc = msc;
+        material.setFloat("Position", msc.anim.anim[msc.position].position);
+        material.setTexture("ColorMap", msc.anim.anim[msc.position].sheetX);
+        material.setFloat("FlipHorizontal", 0.0f);
+        material.setFloat("AlphaValue", 1.0f);
+        material.setColor("FogColor", fogColor);
+        material.setFloat("FogIntensity", 0.0f);
+        material.setFloat("HueShift", hueShift);
     }
 
-    public void setVertexSheetPos(boolean b){
+    public void setVertexSheetPos(boolean b) {
         material.setBoolean("VertexSheetPos", b);
     }
 
@@ -81,9 +80,9 @@ public class MSMaterialControl extends AbstractControl {
 
     }
 
-    public void setFlipped(boolean flipped){
-        this.flipped=flipped;
-        material.setFloat("FlipHorizontal", flipped?1.0f:0.0f);
+    public void setFlipped(boolean flipped) {
+        this.flipped = flipped;
+        material.setFloat("FlipHorizontal", flipped ? 1.0f : 0.0f);
     }
 
     public void setHueShift(float hueShift) {
@@ -91,18 +90,18 @@ public class MSMaterialControl extends AbstractControl {
         material.setFloat("HueShift", hueShift);
     }
 
-    public void setAlpha(float alphaValue){
-        this.alphaValue=alphaValue;
+    public void setAlpha(float alphaValue) {
+        this.alphaValue = alphaValue;
         material.setFloat("AlphaValue", alphaValue);
     }
 
-    public void setFogColor(ColorRGBA fogColor){
-        this.fogColor=fogColor;
+    public void setFogColor(ColorRGBA fogColor) {
+        this.fogColor = fogColor;
         material.setColor("FogColor", fogColor);
     }
 
-    public void setFogIntensity(float fogIntensity){
-        this.fogIntensity=fogIntensity;
+    public void setFogIntensity(float fogIntensity) {
+        this.fogIntensity = fogIntensity;
         material.setFloat("FogIntensity", fogIntensity);
     }
 
